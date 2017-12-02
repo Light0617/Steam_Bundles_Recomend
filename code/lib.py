eff2fdfa339ee7be26fb9c553ef680f99d77afe6
光 <light@guangdeMacBook-Pro.local>
2017-12-01 20:46:53 -0800

    add price model on code/bpr.ipynb

--- code/lib.py
+++ code/lib.py
@@ -240,7 +240,7 @@ class BPR_Item(object):
         sys.stderr.write("\n")
         sys.stderr.flush()
         return numpy.mean(auc_values)
-def get_items(bundle_id, max_bundle_size, index):
+def get_items(bundle_id, max_bundle_size, index, bundle_item_map):
     item=list(bundle_item_map[bundle_id]);
     for i in range(len(item),max_bundle_size):
         item.append(index)
@@ -590,3 +590,195 @@ def get_bundle_rank(user, new_bundle, bundle_item_map, bundle_diversity_map):
   
     rank = np.sum([1.0 if p>0 else 0.0 for p in pref_score])
     return rank
+# theano-bpr
+#
+# Copyright (c) 2014 British Broadcasting Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import theano, numpy
+import theano.tensor as T
+import time
+import sys
+from collections import defaultdict
+
+class BPR_ItemP(object):
+
+    def __init__(self, rank, n_users, n_items, n_tags, tag_array, lambda_u = 0.0025, lambda_i = 0.0025, lambda_j = 0.00025, lambda_d = 0.0025, lambda_p = 0.00025, lambda_q = 0.00025, lambda_bias = 0.0, learning_rate = 0.05):
+        
+        self._rank = rank
+        self._n_users = n_users
+        self._n_items = n_items
+        self._n_tags = n_tags
+        self._lambda_u = lambda_u
+        self._lambda_i = lambda_i
+        self._lambda_j = lambda_j
+        self._lambda_d = lambda_d
+        self._lambda_p = lambda_p
+        self._lambda_q = lambda_q
+        self._lambda_bias = lambda_bias
+        self._learning_rate = learning_rate
+        self.TG = theano.shared(tag_array.astype('float32'), name='TG')
+        self._configure_theano()
+        self._generate_train_model_function()
+        self._generate_test_model_function()
+
+    def _configure_theano(self):
+        """
+          Configures Theano to run in fast mode
+          and using 32-bit floats. 
+        """
+        theano.config.mode = 'FAST_RUN'
+        theano.config.floatX = 'float32'
+
+    def _generate_train_model_function(self):
+        u = T.lvector('u')
+        i = T.lvector('i')
+        j = T.lvector('j')
+
+        h = numpy.random.random((self._n_items, self._rank))
+        b = numpy.random.random(self._n_items)#1*n_items
+        x = numpy.random.random((self._n_users, self._n_tags))
+#         tg = numpy.random.random((self._n_items, self._n_tags))
+        #### needs change ###
+        
+        self.W = theano.shared(numpy.random.random((self._n_users, self._rank)).astype('float32'), name='W')#P
+        self.H = theano.shared(h.astype('float32'), name='H')#Q
+        self.theta = theano.shared(x.astype('float32'), name='theta')
+        self.B = theano.shared(b.astype('float32'), name='B')#beta
+#         self.TG = theano.shared(tg.astype('float32'), name='TG')
+
+        
+#         A = theano.shared(tag_matrix.astype("int32"))
+        #nd_list = numpy.asarray(item_data)
+        #theano_item_data = theano.shared(nd_list)
+        #self.A_i = theano.shared(get_feat(theano_item_data[i]['tags']),name='A_i')
+        #self.A_j = theano.shared(get_feat(theano_item_data[j]['tags']),name='A_j')
+        
+        #x_ui = B_i+P_u*Q_i+X_u*A_i
+        self.W[u]
+        x_ui = T.dot(self.W[u], self.H[i].T).diagonal() + self.B[i] + T.dot(self.theta[u], self.TG[i].T).diagonal()
+        x_uj = T.dot(self.W[u], self.H[j].T).diagonal() + self.B[j] + T.dot(self.theta[u], self.TG[j].T).diagonal()
+        x_uij = T.nnet.sigmoid(x_ui-x_uj)
+        
+        obj = T.sum(T.log(x_uij) - self._lambda_u * (self.W[u] ** 2).sum(axis=1) - 
+                    self._lambda_i * (self.H[i] ** 2).sum(axis=1) - self._lambda_j * 
+                    (self.H[j] ** 2).sum(axis=1) - self._lambda_bias * 
+                    (self.B[i] ** 2 + self.B[j] ** 2) - self._lambda_d * (self.theta[u] ** 2).sum(axis=1))
+        
+        T.printing.Print('still training')
+    
+        cost = - obj
+
+        g_cost_W = T.grad(cost=cost, wrt=self.W)
+        g_cost_H = T.grad(cost=cost, wrt=self.H)
+        g_cost_B = T.grad(cost=cost, wrt=self.B)
+        g_cost_theta = T.grad(cost=cost, wrt=self.theta)
+
+        updates = [(self.W, self.W - self._learning_rate * g_cost_W), (self.H, self.H - self._learning_rate * g_cost_H), 
+                   (self.B, self.B - self._learning_rate * g_cost_B), (self.theta, self.theta - self._learning_rate * g_cost_theta)]
+        
+                                                  #u, i_p, i_n    
+        self.train_model = theano.function(inputs=[u, i, j], outputs=cost, updates=updates)
+
+    
+    def train(self, s_users=None, s_pos_items=None, s_neg_items=None, batch_size=1000):
+        """
+          Trains the BPR Matrix Factorisation model using Stochastic
+          Gradient Descent and minibatches over `train_data`.
+
+          `train_data` is an array of (user_index, item_index) tuples.
+
+          We first create a set of random samples from `train_data` for 
+          training, of size `epochs` * size of `train_data`.
+
+          We then iterate through the resulting training samples by
+          batches of length `batch_size`, and run one iteration of gradient
+          descent for the batch.
+        """
+        if len(s_pos_items) < batch_size:
+            sys.stderr.write("WARNING: Batch size is greater than number of training samples, switching to a batch size of %s\n" % str(len(train_data)))
+            batch_size = len(s_pos_items)
+            
+        sgd_users, sgd_pos_items, sgd_neg_items = s_users, s_pos_items, s_neg_items
+        n_sgd_samples = len(s_users)
+        
+        z = 0
+        t2 = t1 = t0 = time.time()
+        while (z+1)*batch_size < n_sgd_samples:
+            self.train_model(
+                sgd_users[z*batch_size: (z+1)*batch_size],
+                sgd_pos_items[z*batch_size: (z+1)*batch_size],
+                sgd_neg_items[z*batch_size: (z+1)*batch_size]
+            )
+            z += 1
+            t2 = time.time()
+            sys.stderr.write("\rProcessed %s ( %.2f%% ) in %.4f seconds" %(str(z*batch_size), 100.0 * float(z*batch_size)/n_sgd_samples, t2 - t1))
+            sys.stderr.flush()
+            t1 = t2
+        if n_sgd_samples > 0:
+            sys.stderr.write("\nTotal training time %.2f seconds; %e per sample\n" % (t2 - t0, (t2 - t0)/n_sgd_samples))
+            sys.stderr.flush()
+            
+    def _generate_test_model_function(self):
+        """
+          Computes item predictions for `user_index`.
+          Returns an array of prediction values for each item
+          in the dataset.
+        """
+        u = T.lvector('u')
+        i = T.lvector('i')
+        j = T.lvector('j')
+        
+        x_ui = T.dot(self.W[u], self.H[i].T).diagonal() + self.B[i] + T.dot(self.theta[u], self.TG[i].T).diagonal()
+        x_uj = T.dot(self.W[u], self.H[j].T).diagonal() + self.B[j] + T.dot(self.theta[u], self.TG[j].T).diagonal()
+        x_uij = x_ui-x_uj
+        
+        self.test_model = theano.function(inputs=[u, i, j], outputs=x_uij)
+   
+    def test_bundle(self, sgd_users, sgd_pos_items, sgd_neg_items, batch_size=1000):
+        """
+          Computes the Area Under Curve (AUC) on `test_data`.
+
+          `test_data` is an array of (user_index, item_index) tuples.
+
+          During this computation we ignore users and items
+          that didn't appear in the training data, to allow
+          for non-overlapping training and testing sets.
+        """
+        
+        auc_values = []
+        z = 0
+        t2 = t1 = t0 = time.time()
+        n_sgd_samples = len(sgd_users)
+        while (z+1)*batch_size < n_sgd_samples:
+            pref_list=self.test_model(
+                sgd_users[z*batch_size: (z+1)*batch_size],
+                sgd_pos_items[z*batch_size: (z+1)*batch_size],
+                sgd_neg_items[z*batch_size: (z+1)*batch_size]
+            )
+            z += 1
+            t2 = time.time()
+            sys.stderr.write("\rProcessed %s ( %.2f%% ) in %.4f seconds" %(str(z*batch_size), 100.0 * float(z*batch_size)/n_sgd_samples, t2 - t1))
+            t1 = t2
+            
+            auc = np.sum([1.0 if a>0.0 else 0.0 for a in pref_list])
+            auc /= batch_size
+            
+            auc_values.append(auc)
+            sys.stderr.write("\rCurrent AUC mean (%s samples): %0.5f" % (str(z*batch_size), numpy.mean(auc_values)))
+            sys.stderr.flush()
+        
+        sys.stderr.write("\n")
+        sys.stderr.flush()
+        return numpy.mean(auc_values)
